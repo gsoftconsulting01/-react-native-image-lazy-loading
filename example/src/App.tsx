375fdde3abb65436fff6875bf41a8ffadd9e9903
Mac User <macuser@Macs-MacBook-Pro.local>
2021-09-14 16:25:11 +0500

    feat: inital commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,118 @@
-import * as React from 'react';
+import React from 'react';
+import { Dimensions, FlatList, SafeAreaView, StyleSheet } from 'react-native';
+import Image from 'react-native-image-lazy-loading';
 
-import { StyleSheet, View, Text } from 'react-native';
-import ImageLazyLoading from 'react-native-image-lazy-loading';
+const MarvelList = [
+  { id: 1, name: 'Black Panter' },
+  { id: 2, name: 'Black Widow' },
+  { id: 3, name: 'Captain America' },
+  { id: 4, name: 'The Collector' },
+  { id: 5, name: 'Doctor Strange' },
+  { id: 6, name: 'Hawkeye' },
+  { id: 7, name: 'Heimdall' },
+  { id: 8, name: 'Hela' },
+  { id: 9, name: 'Hulk' },
+  { id: 10, name: 'Iron Man' },
+  { id: 11, name: 'Loki' },
+  { id: 12, name: 'Nick Fury' },
+  { id: 13, name: 'Falcon' },
+  { id: 14, name: 'Wanda Maximoff' },
+  { id: 15, name: 'Shocker' },
+  { id: 16, name: 'Spider-Man' },
+  { id: 17, name: 'Thanos' },
+  { id: 18, name: 'Thor' },
+  { id: 19, name: 'Valkyrie' },
+  { id: 20, name: 'Vision' },
+  { id: 21, name: 'Vulture' },
+  { id: 22, name: 'War Machine' },
+  { id: 23, name: 'Winter Solder' },
+  { id: 1, name: 'Black Panter' },
+  { id: 2, name: 'Black Widow' },
+  { id: 3, name: 'Captain America' },
+  { id: 4, name: 'The Collector' },
+  { id: 5, name: 'Doctor Strange' },
+  { id: 6, name: 'Hawkeye' },
+  { id: 7, name: 'Heimdall' },
+  { id: 8, name: 'Hela' },
+  { id: 9, name: 'Hulk' },
+  { id: 10, name: 'Iron Man' },
+  { id: 11, name: 'Loki' },
+  { id: 12, name: 'Nick Fury' },
+  { id: 13, name: 'Falcon' },
+  { id: 14, name: 'Wanda Maximoff' },
+  { id: 15, name: 'Shocker' },
+  { id: 16, name: 'Spider-Man' },
+  { id: 17, name: 'Thanos' },
+  { id: 18, name: 'Thor' },
+  { id: 19, name: 'Valkyrie' },
+  { id: 20, name: 'Vision' },
+  { id: 21, name: 'Vulture' },
+  { id: 22, name: 'War Machine' },
+  { id: 23, name: 'Winter Solder' },
+  { id: 1, name: 'Black Panter' },
+  { id: 2, name: 'Black Widow' },
+  { id: 3, name: 'Captain America' },
+  { id: 4, name: 'The Collector' },
+  { id: 5, name: 'Doctor Strange' },
+  { id: 6, name: 'Hawkeye' },
+  { id: 7, name: 'Heimdall' },
+  { id: 8, name: 'Hela' },
+  { id: 9, name: 'Hulk' },
+  { id: 10, name: 'Iron Man' },
+  { id: 11, name: 'Loki' },
+  { id: 12, name: 'Nick Fury' },
+  { id: 13, name: 'Falcon' },
+  { id: 14, name: 'Wanda Maximoff' },
+  { id: 15, name: 'Shocker' },
+  { id: 16, name: 'Spider-Man' },
+  { id: 17, name: 'Thanos' },
+  { id: 18, name: 'Thor' },
+  { id: 19, name: 'Valkyrie' },
+  { id: 20, name: 'Vision' },
+  { id: 21, name: 'Vulture' },
+  { id: 22, name: 'War Machine' },
+  { id: 23, name: 'Winter Solder' },
+];
 
-export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+const myKeyExtractor = (item) => {
+  return item.id;
+};
 
-  React.useEffect(() => {
-    ImageLazyLoading.multiply(3, 7).then(setResult);
-  }, []);
+const renderItem = ({ index }) => {
+  let source = {
+    uri: 'https://ak.picdn.net/shutterstock/videos/13318067/thumb/1.jpg?ip=x480',
+  };
 
+  return <Image source={source} style={styles.image} />;
+};
+const Marvel = () => {
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <SafeAreaView style={styles.separateHero}>
+      <FlatList
+        data={MarvelList}
+        renderItem={renderItem}
+        keyExtractor={myKeyExtractor}
+        numColumns={2}
+      />
+    </SafeAreaView>
   );
-}
-
+};
+const { width, height } = Dimensions.get('window');
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  separator: {
+    height: 1,
+    width: '100%',
+    backgroundColor: '#ff0000',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  separateHero: {
+    flex: 1,
+  },
+  image: {
+    width: width / 2,
+    height: width / 2,
+    borderWidth: 1,
+    borderColor: 'white',
   },
 });
+
+export default Marvel;
